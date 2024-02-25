acf307f6509d4c7726e8c7330defebdea3adac0e
Anthony Mason <anthony81799@gmail.com>
2024-02-24 22:27:49 -0500

    Disable Arrow keys in nvim normal and visual modes.

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,4 +1,8 @@
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<up>', '<Nop>', { noremap = true })
+vim.keymap.set({ 'n', 'v' }, '<down>', '<Nop>', { noremap = true })
+vim.keymap.set({ 'n', 'v' }, '<left>', '<Nop>', { noremap = true })
+vim.keymap.set({ 'n', 'v' }, '<right>', '<Nop>', { noremap = true })
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
