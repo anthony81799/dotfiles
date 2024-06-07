54f47cbcbb68bf5118bad6d368266b692504db0f
Anthony Mason <anthony81799@gmail.com>
2024-06-07 15:19:59 -0400

    Add wallpapers

--- scripts/rust.sh
+++ scripts/rust.sh
@@ -3,6 +3,6 @@
 # Install Rust once main install is finished.
 echo "Install Rust and desired crates."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-cargo install atuin bacon bat bottom cargo-update eza fd-find gitui ripgrep sd tealdeer topgrade zoxide
+cargo install atuin bacon bat bottom cargo-update eza fd-find gitui ripgrep sd tealdeer topgrade wallust zoxide
 cargo install --locked yazi-fm yazi-cli
 cargo install --locked zellij
