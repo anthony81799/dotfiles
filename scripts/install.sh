f139845b8938e10777a7fa05ed0a225c0a302982
Anthony Mason <anthony81799@gmail.com>
2025-09-05 16:58:18 -0400

    Install docker, lazygit, and luarocks

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Installing package dependencies."
-sudo dnf install zsh autojump-zsh perl jq fastfetch alsa-lib-devel entr fzf git-all neovim openssl-devel python3-pip protobuf protobuf-c protobuf-compiler protobuf-devel cmake zlib-ng zlib-ng-devel oniguruma-devel -y
+sudo dnf install zsh autojump-zsh perl jq fastfetch alsa-lib-devel entr fzf git-all neovim openssl-devel python3-pip protobuf protobuf-c protobuf-compiler protobuf-devel cmake zlib-ng zlib-ng-devel oniguruma-devel luarocks -y
 
 echo "Add RPMFusion."
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
@@ -32,7 +32,7 @@ chsh -s /bin/zsh
 
 echo "Install Oh My Posh."
 if [ ! -d ~/.local/bin ]; then
-  mkdir -p ~/.local/bin;
+  mkdir -p ~/.local/bin
 fi
 curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
 
@@ -41,6 +41,25 @@ sudo dnf install nodejs-npm -y
 sudo npm config set prefix=${XDG_DATA_HOME}/npm
 sudo npm config set cache=${XDG_CACHE_HOME}/npm
 sudo npm config set init-module=${XDG_CONFIG_HOME}/npm/config/npm-init.js
+
+echo "Install Docker"
+sudo dnf remove docker \
+  docker-client \
+  docker-client-latest \
+  docker-common \
+  docker-latest \
+  docker-latest-logrotate \
+  docker-logrotate \
+  docker-selinux \
+  docker-engine-selinux \
+  docker-engine
+
+sudo dnf -y install dnf-plugins-core
+sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl enable --now docker
 
 sudo chmod 777 '.cache'
 sudo chown -R 1000:1000 "/home/amason/.cache/npm"
--- scripts/rust.sh
+++ scripts/rust.sh
@@ -4,18 +4,18 @@
 UPDATE_HELIX=false
 while getopts "h-:u-:" opt; do
   case $opt in
-    h)
+  h)
+    echo "Usage: $0 [--help] [--update-helix]"
+    exit 0
+    ;;
+  u) UPDATE_HELIX=true ;;
+  -) case $OPTARG in
+    help)
       echo "Usage: $0 [--help] [--update-helix]"
       exit 0
-    ;;
-    u) UPDATE_HELIX=true ;;
-    -) case $OPTARG in
-         help)
-           echo "Usage: $0 [--help] [--update-helix]"
-           exit 0
-         ;;
-         update-helix) UPDATE_HELIX=true ;;
-       esac
+      ;;
+    update-helix) UPDATE_HELIX=true ;;
+    esac ;;
   esac
 done
 
@@ -23,10 +23,11 @@ done
 echo "Install Rust and desired crates."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cargo install atuin bacon bat bottom cargo-update eza fd-find ripgrep sd tealdeer topgrade wallust zoxide
-cargo install --locked gitui
+# cargo install --locked gitui
 cargo install --locked yazi-fm yazi-cli
 cargo install --locked zellij
 cargo install --locked dysk
+cargo install --locked ast-grep
 
 echo "Creating repos folder and cloning Helix"
 mkdir ~/repos
