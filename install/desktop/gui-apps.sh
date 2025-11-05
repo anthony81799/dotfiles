91295f46cc546ad72abbb53d33ff7d84e7994130
Anthony Mason <anthony81799@gmail.com>
2025-11-05 12:07:28 -0500

    Add Brave repo correctly

--- install/desktop/gui-apps.sh
+++ install/desktop/gui-apps.sh
@@ -43,7 +43,7 @@ okay_message "VS Code installed."
 # ------------------------------------------------------------
 spinner "Installing Brave Browser repository..."
 sudo dnf install -y dnf-plugins-core
-sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+sudo dnf config-manager addrepo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 
 spinner "Installing Brave Browser..."
