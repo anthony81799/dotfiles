c7bdc466cfec13eecf539a7aafa0a1ec91dbc498
Anthony Mason <anthony81799@gmail.com>
2025-11-20 09:19:26 -0500

    Add xplr config

---
+++ .config/xplr/init.lua
@@ -0,0 +1,6 @@
+version = '1.0.1'
+---@diagnostic disable
+local xplr = xplr -- The globally exposed configuration to be overridden.
+---@diagnostic enable
+
+xplr.config.general.initial_layout = "no_help"
